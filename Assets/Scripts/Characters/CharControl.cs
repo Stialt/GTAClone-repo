d013d31ca4f9835bb87fba156716d197eac36580
Stialt <34300269+Stialt@users.noreply.github.com>
2020-03-11 09:09:47 +0900

    2020.3.11
    # Tutorial 2
     - Asphalt texture, material and normal map created
     - Sidewalk texture, material and normal map created
     - OpenWorld scene created
    
    # Tutorial 3
     - Contract Killer package is imported
     - Contract Killer default character is placed on the OpenWorld scene
     - Contract Killer default animations and animator removed
     - Created new animation for Contract Killer which includes: Idle, Walk and Run legacy animations from package
    
    # Tutorial 4
     - Created Character Controlling Script which allows to turn and move backwards or forward
    
    # Tutorial 5
     - Imported "Shipping Container Environment" package
     - Added more objects on scene
    
    # Tutorial 6
     - Changed Scene by adding walls, changing textures
     - Added particles to immitate dust
     - Added new camera to be used for Intro Sequence later
    
    # Tutorial 7
     - Added animation for second camera to rotate around scene
     - Added script for cameras sequences

---
+++ Assets/Scripts/Characters/CharControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharControl : MonoBehaviour
+{
+    public GameObject thePlayer;
+    public bool isRunning;
+    public float horizontalMove, verticalMove;
+
+    void Update()
+    {
+        if (Input.GetButton("Horizontal") ||
+            Input.GetButton("Vertical"))
+        {
+            thePlayer.GetComponent<Animation>()
+                .Play("Run");
+            horizontalMove = Input.GetAxis("Horizontal") 
+                * Time.deltaTime * 150;
+            verticalMove = Input.GetAxis("Vertical") * Time.deltaTime * 8;
+            isRunning = true;
+            transform.Rotate(0, horizontalMove, 0);
+            transform.Translate(0, 0, verticalMove);
+        
+        }
+        else
+        {
+            thePlayer.GetComponent<Animation>().Play("Idle");
+            isRunning = false;
+        }
+    }
+}
