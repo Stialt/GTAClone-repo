6ea4c77842d47a267ca2c67f1e3378c513e86974
Stialt <34300269+Stialt@users.noreply.github.com>
2020-03-12 09:48:47 +0900

    2020.03.12
    ### Tutorial 8
    - Changed Skybox
    - Added another directional light to scene
    - Added additional character clones for walking sequence
    - Set up Walking sequence
    - Imported Wooden Chair Package
    - Added Chair
    
    ### Tutorial 9
    - Imported Animatii Package
    - Added sitting policeman on the chair (Vlei)
    - Added sitting animation to Vlei
    
    ### Tutorial 10
    - Added barrel bag on Vlei's head
    - Added seagul, wind and foghorn sounds
    - Added more titles and subtitles

---
+++ Assets/Scripts/IntroScene/A02_MoveChar.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class A02_MoveChar : MonoBehaviour
+{
+    public AudioSource leftFoot;
+    public AudioSource rightFoot;
+    public bool steppingLeft = true;
+    public GameObject mainChar;
+    public int stepsTaken = 0;
+
+
+    void Start()
+    {
+        StartCoroutine(WalkSequence());
+    }
+
+    void Update()
+    {
+        mainChar.transform.Translate(0, 0, 0.014f * Time.timeScale);
+    }
+
+    IEnumerator WalkSequence()
+    {
+        //yield return new WaitForSeconds(0.4f);
+        while(stepsTaken < 12)
+        {
+            yield return new WaitForSeconds(0.5f);
+            if (steppingLeft)
+            {
+                leftFoot.Play();
+                steppingLeft = false;
+            }
+            else
+            {
+                rightFoot.Play();
+                steppingLeft = true;
+            }
+            stepsTaken++ ;
+        }
+        mainChar.SetActive(false);
+    }
+}
